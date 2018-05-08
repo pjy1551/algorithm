f84366cf4a9cee336520e557361ef68db2f8ca14
박주영 <pjy1551@douzone.com>
2018-05-08 15:07:45 +0900

    설탕배달 알고리즘 문제

---
+++ src/Backjoon2839.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Backjoon2839 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		int calc;
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int temp = input / 3;
+		int temp2 = input / 5;
+		for(int i=0; i <= temp; i++)
+		{
+			for(int j=0; j <= temp2; j++ )
+			{
+				calc = 3*i + 5*j;
+				if(calc == input) {
+					list.add(i+j);
+				}
+			}
+		}
+		if(list.size() > 0) 
+		{
+			System.out.println(Collections.min(list));
+		}
+		else
+		{
+			System.out.println(-1);
+		}
+
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,6 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int input = scanner.nextInt();
-		int calc;
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		int temp = input / 3;
-		int temp2 = input / 5;
-		for(int i=0; i <= temp; i++)
-		{
-			for(int j=0; j <= temp2; j++ )
-			{
-				calc = 3*i + 5*j;
-				if(calc == input) {
-					list.add(i+j);
-				}
-			}
-		}
-		if(list.size() > 0) 
-		{
-			System.out.println(Collections.min(list));
-		}
-		else
-		{
-			System.out.println(-1);
-		}
+
 	}
 }
