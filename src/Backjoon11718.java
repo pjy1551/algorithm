029966b03d9b4000f1342ecb468df32a4947d2d1
박주영 <pjy1551@douzone.com>
2018-05-09 16:03:39 +0900

    그대로 출력하기

---
+++ src/Backjoon11718.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Backjoon11718 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String result;
+		while(scanner.hasNext()) {
+			result = scanner.nextLine();
+			System.out.println(result);
+		}
+
+	}
+
+}
