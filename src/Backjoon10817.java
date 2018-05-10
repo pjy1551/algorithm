32187fc13d3719567ebcd959babbff2f6725ee19
박주영 <pjy1551@douzone.com>
2018-05-10 11:40:07 +0900

    세 수

---
+++ src/Backjoon10817.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Backjoon10817 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		list.add(scanner.nextInt());
+		list.add(scanner.nextInt());
+		list.add(scanner.nextInt());
+		
+		list.sort(null);;
+		
+		System.out.println(list.get(1));
+		
+	}
+}
