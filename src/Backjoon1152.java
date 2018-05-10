e995c553710a068af7e53113c3aef1360d758361
박주영 <pjy1551@douzone.com>
2018-05-10 16:59:11 +0900

    단어의 개수

--- src/Backjoon1152.java
+++ src/Backjoon1152.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class Backjoon1152 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String a = sc.nextLine();
-		String[] worlds = a.split("\\s");
-		int count = 0;
-		for( String result : worlds)
-		{
-			count++;
-			if(result.equals("")) count--;
-		}
-		System.out.println(count);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String[] worlds = a.split("\\s");
+        int count = 0;
+        for (String result : worlds) {
+            count++;
+            if (result.equals(""))
+                count--; 
+        }
+        System.out.println(count);
 
 	}
 
