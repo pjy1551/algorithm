e157cd81e45159c6a71e28784e9a367b25b4ba5b
박주영 <pjy1551@douzone.com>
2018-05-11 17:15:09 +0900

    N 찍기

--- src/BackJoon2741.java
+++ src/BackJoon2741.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class BackJoon2741 {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int a = scanner.nextInt();
-		
-		for(int i = 1; i <= a; i++){
-			System.out.println(i);
-		}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
 
-	}
+        for (int i = 1; i <= a; i++) {
+            System.out.println(i);
+        }
+
+    }
 
 }
