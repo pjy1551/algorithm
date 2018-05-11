a47c76f5dceccaacf245dc562378473ac5dce0cd
박주영 <pjy1551@douzone.com>
2018-05-11 17:13:34 +0900

    A+B

--- src/Backjoon1000.java
+++ src/Backjoon1000.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class Backjoon1000 {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
-		int result = 0;
-		
-		result = a+b;
-		
-		System.out.println(result);
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int result = 0;
+
+        result = a + b;
+
+        System.out.println(result);
+    }
 
 }
