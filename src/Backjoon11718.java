77b76cfaf56fa4b1a4f5da691bbc660fdc71b0eb
박주영 <pjy1551@douzone.com>
2018-05-11 17:16:30 +0900

    그대로 출력하기

--- src/Backjoon11718.java
+++ src/Backjoon11718.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Backjoon11718 {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String result;
-		while(scanner.hasNext()) {
-			result = scanner.nextLine();
-			System.out.println(result);
-		}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String result;
+        while (scanner.hasNext()) {
+            result = scanner.nextLine();
+            System.out.println(result);
+        }
 
-	}
+    }
 
 }
