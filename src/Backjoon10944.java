8ba43203c4f75e62b18785f7a7671276e6b2ba46
박주영 <pjy1551@douzone.com>
2018-05-11 17:18:53 +0900

    랜덤 게임~~

---
+++ src/Backjoon10944.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Backjoon10944 {
+
+    public static void main(String[] args) {
+        Random a = new Random();
+
+        int i = a.nextInt(10000);
+        System.out.println(i);
+        
+    }
+
+}
