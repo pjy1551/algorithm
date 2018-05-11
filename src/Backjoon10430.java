0e35e3c1f0054ea415bdba3c38a9aa13afb7a95c
박주영 <pjy1551@douzone.com>
2018-05-11 17:15:51 +0900

    나머지

--- src/Backjoon10430.java
+++ src/Backjoon10430.java
@@ -7,11 +7,11 @@ public class Backjoon10430 {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        
-        System.out.println((a+b)%c);
-        System.out.println((a%c + b%c)%c);
-        System.out.println((a*b)%c);
-        System.out.println((a%c * b%c)%c);
+
+        System.out.println((a + b) % c);
+        System.out.println((a % c + b % c) % c);
+        System.out.println((a * b) % c);
+        System.out.println((a % c * b % c) % c);
 
     }
 
