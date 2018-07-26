816adca954c4d0766f6b02befe5d4614bf873637
박주영 <pjy1551@douzone.com>
2018-07-26 11:16:12 +0900

    별찍기 - 2

--- src/Backjoon2439.java
+++ src/Backjoon2439.java
@@ -4,14 +4,17 @@ public class Backjoon2439 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt();
-        for (int i = a; i > 0; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+        int input = sc.nextInt();
+        for(int i = input; i >= 1; i--) {
+            for(int j = 1; j <= input; j++) {
+                if(j < i ) {
+                    System.out.print(" ");
+                }
+                else {
+                    System.out.print("*");
+                }
             }
             System.out.println();
         }
-
     }
 }
