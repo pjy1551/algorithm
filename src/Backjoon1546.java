33e6ce46164ea618dbf4e3ce034f0f39be8ad27e
박주영 <pjy1551@douzone.com>
2018-07-26 10:59:39 +0900

    평균

---
+++ src/Backjoon1546.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Backjoon1546 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        List<Double> list = new ArrayList<>();
+        double sum = 0;
+        
+        for(int i = 0; i < count; i++) {
+           list.add(sc.nextDouble());
+        }
+        // �ִ밪
+        Double max = Collections.max(list);
+        
+        for(int i = 0; i< count; i++) {
+            list.set(i, list.get(i) / max * 100);
+            sum += list.get(i);
+         }
+        System.out.printf("%.2f", sum / list.size());
+
+    }
+
+}
