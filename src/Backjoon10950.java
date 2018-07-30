9cce183f9c388d6890d5b6b23a4236b53f938ace
박주영 <pjy1551@douzone.com>
2018-07-30 16:14:26 +0900

    A+B - 3

---
+++ src/Backjoon10950.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Backjoon10950 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b, c;
+        for(int i = 0; i < a; i ++) {
+            b = sc.nextInt();
+            c = sc.nextInt();
+            
+            System.out.println(b+c);
+        }
+    }
+
+}
