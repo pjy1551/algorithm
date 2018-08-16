ab65c3e47a39d7ffe5ef3396d8ab3b4325b504ae
박주영 <pjy1551@douzone.com>
2018-08-16 10:48:53 +0900

    아스키 코드

---
+++ src/Backjoon11654.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Backjoon11654 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       int a = 0;
+       String b = sc.nextLine();
+       a = b.charAt(0);
+       System.out.println(a);
+    }
+
+}
