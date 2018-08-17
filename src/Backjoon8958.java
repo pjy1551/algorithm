b9ef90676fedd152e4f9263435c8a11b0fc4107a
pjy1551 <pjy1551@gmail.com>
2018-08-17 15:37:37 +0900

    OX퀴즈

---
+++ src/Backjoon8958.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Backjoon8958 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int sum = 0;
+        
+        for(int i = 0; i < a; i++) {
+            String str = sc.next();
+            char[] charArray = str.toCharArray();
+            int count = 0;
+            
+            for(char ch: charArray) {
+                if(ch == 'O') {
+                    count++;
+                    sum += count;
+                }
+                else {
+                    count = 0;
+                }
+            }
+            System.out.println(sum);
+            sum = 0;
+        }
+
+    }
+
+}
