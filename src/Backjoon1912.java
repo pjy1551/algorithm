f8dbe8a703c9f9dec13b79d7570e7c483999481d
pjy1551 <pjy1551@gmail.com>
2018-08-21 11:47:13 +0900

    연속합

---
+++ src/Backjoon1912.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Backjoon1912 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count, result;
+        int[] dp = new int[100001];
+        Arrays.fill(dp, -12345);
+        
+        count = sc.nextInt();
+        for(int i = 0; i < count; i++) {
+            dp[i] = sc.nextInt();
+        }
+        result = dp[0];
+        for(int i = 1; i <= count; i++) {
+            dp[i] = Math.max(dp[i], dp[i] + dp[i-1]);
+            result = Math.max(result, dp[i]);
+        }
+        System.out.println(result);
+        
+        
+    }
+
+}
