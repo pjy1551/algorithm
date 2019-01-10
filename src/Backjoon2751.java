4dab6acf234de73969b6e4d05f9af62e559e4de6
pjy1551 <pjy1551@gmail.com>
2019-01-10 14:12:56 +0900

    수 정렬하기 2

--- src/Backjoon2751.java
+++ src/Backjoon2751.java
@@ -1,25 +1,58 @@
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Backjoon2751 {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        List<Integer> list = new ArrayList<Integer>();
-        for(int i = 0; i < number; i++) {
-            list.add(sc.nextInt());
+    public static int result[];
+   
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine().trim());
+        result = new int[N];
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine().trim());
         }
-        list.sort(null);
-        
-        List<Integer> duplicateRemoveList = new ArrayList<Integer>(new LinkedHashSet<Integer>(list));
-        
-        for(int j = 0; j < duplicateRemoveList.size(); j++) {
-            System.out.println(duplicateRemoveList.get(j));
+        mergeSort(0, N-1, arr);
+        for(int i = 0; i <N; i++) {
+            sb.append(result[i]+"\n");
         }
-
+        System.out.println(sb);
     }
-
+    // 병합정렬
+    public static void mergeSort(int left, int right, int[] arr) {
+        int mid;
+        if(left < right) {
+            mid = (left+right)/2;
+            mergeSort(left, mid, arr);
+            mergeSort(mid+1, right, arr);
+            merge(left, mid, right, arr);
+         }
+    }
+    // 머지하기
+    public static void merge(int left, int mid, int right, int[] arr) {
+        int l = left;
+        int m = mid+1;
+        int k = left;
+        
+        while(l <= mid || m <= right) {
+            if(l <= mid && m <= right) {
+                if(arr[l] <= arr[m]) {
+                    result[k] = arr[l++];
+                }
+                else {
+                    result[k] = arr[m++];
+                }
+            }else if (l <= mid && m > right) {
+                result[k] = arr[l++];
+            }else {
+                result[k] = arr[m++];
+            }
+            k++;
+        }
+        for(int i = left; i < right+1; i++) {
+            arr[i] = result[i];
+        }
+    }
 }
